c2377da7c37b62cef737ad12a8dc7e03a86af8f5
ALPHABANDIT <117475176+ALPHABANDIT2009@users.noreply.github.com>
2025-08-30 09:52:57 -0600

    Added all the current files

---
+++ main_scripts.js
@@ -0,0 +1,25 @@
+// Wait for site to load
+window.addEventListener('load', function () {
+
+
+
+
+
+    // Draggable elements
+
+    // Games tab
+    function dragElement(e){var t=0,n=0,o=0,d=0;function l(l){(l=l||window.event).preventDefault(),t=o-l.clientX,n=d-l.clientY,o=l.clientX,d=l.clientY,e.style.top=e.offsetTop-n+"px",e.style.left=e.offsetLeft-t+"px"}function m(){document.onmouseup=null,document.onmousemove=null,document.getElementById("window_ID_games").classList.remove("Dragged_State"),document.getElementById("window_bar_ID_games").classList.remove("Dragged_State")}document.getElementById("window_bar_ID_games").onmousedown=function(e){(e=e||window.event).preventDefault(),o=e.clientX,d=e.clientY,document.onmouseup=m,document.getElementById("window_ID_games").classList.add("Dragged_State"),document.getElementById("window_bar_ID_games").classList.add("Dragged_State"),document.onmousemove=l}}dragElement(document.getElementById("window_ID_games"));
+   
+})
+
+//App Functions
+function test_app_load() {
+      window.open("https://www.youtube.com/embed/pxEV1A5mTYM", "Test_App", "height=300,width=540");
+}
+
+
+
+// Game Apps
+function nes_load() {
+
+}
