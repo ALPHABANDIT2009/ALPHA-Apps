b51660af947eff8a5d07ff42356ce2185e8fd301
ALPHABANDIT2009 <will.camp.alpha@gmail.com>
2025-08-30 19:58:31 -0600

    Now just polishing. ill polish a little more then actuallly make the emulator code cleaner

--- main_scripts.js
+++ main_scripts.js
@@ -9,17 +9,36 @@ window.addEventListener('load', function () {
 
     // Games tab
     function dragElement(e){var t=0,n=0,o=0,d=0;function l(l){(l=l||window.event).preventDefault(),t=o-l.clientX,n=d-l.clientY,o=l.clientX,d=l.clientY,e.style.top=e.offsetTop-n+"px",e.style.left=e.offsetLeft-t+"px"}function m(){document.onmouseup=null,document.onmousemove=null,document.getElementById("window_ID_games").classList.remove("Dragged_State"),document.getElementById("window_bar_ID_games").classList.remove("Dragged_State")}document.getElementById("window_bar_ID_games").onmousedown=function(e){(e=e||window.event).preventDefault(),o=e.clientX,d=e.clientY,document.onmouseup=m,document.getElementById("window_ID_games").classList.add("Dragged_State"),document.getElementById("window_bar_ID_games").classList.add("Dragged_State"),document.onmousemove=l}}dragElement(document.getElementById("window_ID_games"));
-   
+    document.getElementById("window_ID_games").style.display = "none";
 })
 
-//App Functions
-function test_app_load() {
-      window.open("https://www.youtube.com/embed/pxEV1A5mTYM", "Test_App", "height=300,width=540");
+
+
+
+//Folder Functions
+
+//Game Folder
+function game_folder_load() {
+    document.getElementById("window_ID_games").style.display = "block";
+}
+function game_folder_close() {
+     document.getElementById("window_ID_games").style.display = "none";
 }
 
 
+
+
+
+//App Functions
 
 // Game Apps
 function emulator_load() {
  window.open("Apps/Emulator JS/4.2.3/index.html", "Emulation", "height=500,width=600");
 }
+//Test App
+function test_app_load() {
+      window.open("https://www.youtube.com/embed/FtutLA63Cp8", "Test_App", "height=300,width=540");
+}
+
+
+
